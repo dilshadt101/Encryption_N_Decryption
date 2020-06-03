476e5b6af5589975da3bf8df7ee3817e82f3ba88
dilshadt101 <dilshadt101@gmail.com>
2020-06-03 17:18:43 +0530

    String encryption and decryption

---
+++ String_enc.py
@@ -0,0 +1,70 @@
+from Crypto.Cipher import AES
+from base64 import b64encode, b64decode
+
+
+class Crypt:
+
+    def __init__(self, salt='SlTKeYOpHygTYkP3'):
+        self.salt = salt.encode('utf8')
+        self.enc_dec_method = 'utf-8'
+
+    def encrypt(self, str_to_enc, str_key):
+        try:
+            aes_obj = AES.new(str_key, AES.MODE_CFB, self.salt)
+            hx_enc = aes_obj.encrypt(str_to_enc.encode('utf8'))
+            mret = b64encode(hx_enc).decode(self.enc_dec_method)
+            return mret
+        except ValueError as value_error:
+            if value_error.args[0] == 'IV must be 16 bytes long':
+                raise ValueError('Encryption Error: SALT must be 16 characters long')
+            elif value_error.args[0] == 'AES key must be either 16, 24, or 32 bytes long':
+                raise ValueError('Encryption Error: Encryption key must be either 16, 24, or 32 characters long')
+            else:
+                raise ValueError(value_error)
+
+    def decrypt(self, enc_str, str_key):
+        try:
+            aes_obj = AES.new(str_key.encode('utf8'), AES.MODE_CFB, self.salt)
+            str_tmp = b64decode(enc_str.encode(self.enc_dec_method))
+            str_dec = aes_obj.decrypt(str_tmp)
+            mret = str_dec.decode(self.enc_dec_method)
+            return mret
+        except ValueError as value_error:
+            if value_error.args[0] == 'IV must be 16 bytes long':
+                raise ValueError('Decryption Error: SALT must be 16 characters long')
+            elif value_error.args[0] == 'AES key must be either 16, 24, or 32 bytes long':
+                raise ValueError('Decryption Error: Encryption key must be either 16, 24, or 32 characters long')
+            else:
+                raise ValueError(value_error)
+
+
+def main():
+    test_crpt = Crypt()
+    test_text = """Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum."""
+
+    test_key = 'MyKey4TestingYnP'
+    test_enc_text = test_crpt.encrypt(test_text, test_key)
+    test_dec_text = test_crpt.decrypt(test_enc_text, test_key)
+    print(f'Encrypted:{test_enc_text}  Decrypted:{test_dec_text}')
+
+
+
+if __name__ == '__main__':
+    main()
